357c60c940d3f24a2d37056a210714c9008935cf
Pablo Caiza <caizap@hotmail.com>
2020-06-29 21:41:05 -0500

    prueba2

--- database/seeds/AppointmentsTableSeeder.php
+++ database/seeds/AppointmentsTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Appointment;
 
-class AppointmentTableSeeder extends Seeder
+class AppointmentsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
