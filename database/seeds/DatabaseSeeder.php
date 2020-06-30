8da613828544c3efc1ea66292d235a550e3efb48
Pablo Caiza <caizap@hotmail.com>
2020-06-29 20:51:09 -0500

    yaff

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
           $this->call([
-            UserTableSeeder::class,
+            UsersTableSeeder::class,
             SpecialtiesTableSeeder::class,
             WorkDaysTableSeeder::class,
             AppointmentsTableSeeder::class
